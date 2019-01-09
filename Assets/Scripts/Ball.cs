ec1c6e0135b49cefff3cb055942aedab90205a1e
Jason <jasonchiewsam@gmail.com>
2019-01-09 20:43:40 +0100

    Creating private repository

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public enum BallState { Inactive, NextDisplay, GetStart, Moving }
+    public BallState currentBallState;
+
+    SpriteRenderer spriteRenderer;
+    Vector2 position;
+
+    public float speed;
+    public Transform transf;
+    public Transform targetTransf;
+    public int randomColor;
+
+    public List<Transform> starts;
+    public List<Sprite> colors;
+
+    void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        transf = GetComponent<Transform>();
+        currentBallState = BallState.Inactive;
+	}
+	
+	void Update ()
+    {
+        switch (currentBallState)
+        {
+            case BallState.Inactive:
+                Inactive();
+                break;
+            case BallState.NextDisplay:
+                NextDisplay();
+                break;
+            case BallState.GetStart:
+                GetStart();
+                break;
+            case BallState.Moving:
+                Moving();
+                break;
+            default:
+                break;
+        }
+    }
+
+    #region Updates
+
+    void Inactive()
+    {
+        randomColor = Random.Range(0, 4);
+
+        if (randomColor == 0) spriteRenderer.color = new Color(255, 0, 0);
+        else if (randomColor == 1) spriteRenderer.color = new Color(255, 255, 0);
+        else if (randomColor == 2) spriteRenderer.color = new Color(0, 255, 0);
+        else if (randomColor == 3) spriteRenderer.color = new Color(0, 255, 255);
+        else if (randomColor == 4) spriteRenderer.color = new Color(0, 0, 255);
+
+        //spriteRenderer.sprite = colors[Random.Range(0, 4)];
+
+        currentBallState = BallState.NextDisplay;
+    }
+
+    void NextDisplay()
+    {
+        currentBallState = BallState.GetStart;
+    }
+
+    void GetStart()
+    {
+        transf.localPosition = starts[Random.Range(0, 4)].localPosition;
+        position = transf.localPosition;
+
+        currentBallState = BallState.Moving;
+    }
+
+    void Moving()
+    {
+        transf.localPosition = position;
+        position.y -= speed * Time.deltaTime;
+    }
+
+    #endregion
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("colliding" + collision);
+    }
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool open;
+    public bool cannotClose;
+
+    public GameObject leftPoint;
+    public GameObject rightPoint;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start ()
+    {
+        open = false;
+        cannotClose = false;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        Close();
+	}
+	
+	void Update ()
+    {
+		
+    }
+
+    void OnMouseDown()
+    {
+        if(cannotClose)
+        {
+            return;
+        }
+        else
+        {
+            open = !open;
+            
+            if(open)
+            {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+        }
+    }
+
+    void Open()
+    {
+        leftPoint.SetActive(true);
+        rightPoint.SetActive(true);
+        spriteRenderer.color = new Color(255, 255, 255);
+        Debug.Log("open");
+    }
+
+    void Close()
+    {
+        leftPoint.SetActive(false);
+        rightPoint.SetActive(false);
+        spriteRenderer.color = new Color(0, 0, 0);
+        Debug.Log("close");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ball")
+        {
+            cannotClose = true;
+            Debug.Log("cannot close");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Ball")
+        {
+            cannotClose = false;
+            Debug.Log("can close");
+        }
+    }
+}
